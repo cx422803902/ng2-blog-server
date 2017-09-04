ee946deee47fa2d6b5879f9838a3e1497fa3dea6
cx422803902 <422803902@qq.com>
2017-09-04 14:46:36 +0800

    bootstrap startup

--- src/main/java/org/goiot/boot/BlogBoot.java
+++ src/main/java/org/goiot/boot/BlogBoot.java
@@ -4,16 +4,20 @@ import java.nio.charset.Charset;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizerBeanPostProcessor;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessor;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -23,24 +27,31 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
  * Created by ChenXing on 2017/8/24 0024.
  */
 
-@SpringBootApplication
-@ServletComponentScan(value = "org.goiot")
-@PropertySource(value = "classpath:application.properties")
-@ImportResource(locations = "classpath:/spring/*.xml")
+@SpringBootConfiguration
+@ImportAutoConfiguration(value = {
+		ConfigurationPropertiesBindingPostProcessor.class,
+		TomcatEmbeddedServletContainerFactory.class,
+		EmbeddedServletContainerCustomizerBeanPostProcessor.class,
+		ServerProperties.class})
 @ComponentScan(basePackages = "org.goiot")
+@ImportResource(locations = "classpath:/spring/*.xml")
 public class BlogBoot extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
+		new SpringApplicationBuilder()
+				.sources(BlogBoot.class)
+				.contextClass(AnnotationConfigEmbeddedWebApplicationContext.class)
+				.build()
+				.run(args);
 		SpringApplication.run(BlogBoot.class, args);
 	}
 
 	@Bean
 	public ServletRegistrationBean restServlet() {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-		DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+		DispatcherServlet dispatcherServlet = new DispatcherServlet();
 		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(dispatcherServlet);
 		servletRegistrationBean.setLoadOnStartup(1);
-		servletRegistrationBean.addUrlMappings("/*");
+		servletRegistrationBean.addUrlMappings("/");
 		return servletRegistrationBean;
 	}
 
